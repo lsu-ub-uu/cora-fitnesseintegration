7865bc2a5c1b6baef333248ea27838f672a46ea6
marcus <marcus@d91479a7df28>
2024-09-19 14:57:21 +0000

    Some more tests for definition writer

--- src/main/java/se/uu/ub/cora/fitnesseintegration/definitionwriter/DefinitionWriter.java
+++ src/main/java/se/uu/ub/cora/fitnesseintegration/definitionwriter/DefinitionWriter.java
@@ -5,29 +5,66 @@ import java.util.List;
 import se.uu.ub.cora.data.DataAtomic;
 import se.uu.ub.cora.data.DataChild;
 import se.uu.ub.cora.data.DataGroup;
+import se.uu.ub.cora.data.DataRecordLink;
+import se.uu.ub.cora.data.DataResourceLink;
 
 public class DefinitionWriter {
 
 	private static final String NEW_LINE = "\n";
 	private static final String TAB = "\t";
 	private String definition = "";
+	private int childDepth = 0;
 
 	public String writeDefinitionFromUsingDataChild(DataChild dataChild) {
 		if (isGroup(dataChild)) {
+			// todo: collect all child groups (per level) and process them indiviually to handle all
+			// nested group levels
+			childDepth += 1;
 			DataGroup dataGroup = (DataGroup) dataChild;
 			definition = definition + writeGroup(dataGroup);
 
-			continueWritingDefinitionIfChildrenExists(dataGroup);
+			possiblyTraverseAndWriteChildren(dataGroup);
 		}
+
+		possiblyWriteDataChild(dataChild);
+
+		return definition;
+	}
+
+	private void possiblyWriteDataChild(DataChild dataChild) {
 		if (isAtomic(dataChild)) {
 			DataAtomic dataAtomic = (DataAtomic) dataChild;
 			definition = definition + writeAtomic(dataAtomic);
 		}
 
-		return definition;
+		if (isRecordLink(dataChild)) {
+			DataRecordLink recordLink = (DataRecordLink) dataChild;
+			definition = definition + writeRecordLink(recordLink);
+		}
+
+		if (isResourceLink(dataChild)) {
+			DataResourceLink resourceLink = (DataResourceLink) dataChild;
+			definition = definition + writeResourceLink(resourceLink);
+		}
 	}
 
-	private void continueWritingDefinitionIfChildrenExists(DataGroup dataGroup) {
+	private boolean isGroup(DataChild dataChild) {
+		return dataChild instanceof DataGroup;
+	}
+
+	private boolean isRecordLink(DataChild dataChild) {
+		return dataChild instanceof DataRecordLink;
+	}
+
+	private boolean isResourceLink(DataChild dataChild) {
+		return dataChild instanceof DataResourceLink;
+	}
+
+	private String writeGroup(DataGroup dataGroup) {
+		return dataGroup.getNameInData() + "(group)";
+	}
+
+	private void possiblyTraverseAndWriteChildren(DataGroup dataGroup) {
 		if (dataGroup.hasChildren()) {
 			List<DataChild> children = dataGroup.getChildren();
 			for (DataChild child : children) {
@@ -35,31 +72,33 @@ public class DefinitionWriter {
 				addTab();
 				writeDefinitionFromUsingDataChild(child);
 			}
+			childDepth = 0;
 		}
-	}
-
-	private void addTab() {
-		definition = definition + TAB;
 	}
 
 	private void addNewLine() {
 		definition = definition + NEW_LINE;
 	}
 
+	private void addTab() {
+		for (int i = 0; i < childDepth; i++) {
+			definition = definition + TAB;
+		}
+	}
+
 	private boolean isAtomic(DataChild dataChild) {
 		return dataChild instanceof DataAtomic;
 	}
 
-	private String writeGroup(DataGroup dataGroup) {
-		return dataGroup.getNameInData() + "(group)";
-	}
-
 	private String writeAtomic(DataAtomic dataAtomic) {
-		return dataAtomic.getNameInData() + "(atomic, X-X, someConstarint)";
+		return dataAtomic.getNameInData() + "(textVariable, 1-1, noConstraint)";
 	}
 
-	private boolean isGroup(DataChild dataChild) {
-		return dataChild instanceof DataGroup;
+	private String writeRecordLink(DataRecordLink dataRecordLink) {
+		return dataRecordLink.getNameInData() + "(recordLink, 1-1, noConstraint)";
 	}
 
+	private String writeResourceLink(DataResourceLink dataResourcedLink) {
+		return dataResourcedLink.getNameInData() + "(resourceLink, 1-1, noConstraint)";
+	}
 }
--- src/test/java/se/uu/ub/cora/fitnesseintegration/definitionwriter/DefinitionWriterTest.java
+++ src/test/java/se/uu/ub/cora/fitnesseintegration/definitionwriter/DefinitionWriterTest.java
@@ -9,6 +9,8 @@ import org.testng.annotations.Test;
 
 import se.uu.ub.cora.data.spies.DataAtomicSpy;
 import se.uu.ub.cora.data.spies.DataGroupSpy;
+import se.uu.ub.cora.data.spies.DataRecordLinkSpy;
+import se.uu.ub.cora.data.spies.DataResourceLinkSpy;
 
 public class DefinitionWriterTest {
 
@@ -17,10 +19,15 @@ public class DefinitionWriterTest {
 
 	@BeforeMethod
 	private void beforeMethod() {
-		dataGroup = new DataGroupSpy();
-		dataGroup.MRV.setDefaultReturnValuesSupplier("getNameInData", () -> "someGroup");
+		dataGroup = createDataGroupSpy("someGroup");
 
 		writer = new DefinitionWriter();
+	}
+
+	private DataGroupSpy createDataGroupSpy(String name) {
+		DataGroupSpy dataGroupSpy = new DataGroupSpy();
+		dataGroupSpy.MRV.setDefaultReturnValuesSupplier("getNameInData", () -> name);
+		return dataGroupSpy;
 	}
 
 	@Test
@@ -39,10 +46,74 @@ public class DefinitionWriterTest {
 		dataGroup.MRV.setDefaultReturnValuesSupplier("getChildren", () -> List.of(textVariable));
 
 		String definition = writer.writeDefinitionFromUsingDataChild(dataGroup);
-
 		String expectedDefinition = """
 				someGroup(group)
-				someTextVariable(textVariable, 1-1, noConstraint)""";
+					someTextVariable(textVariable, 1-1, noConstraint)""";
+		assertEquals(definition, expectedDefinition);
+	}
+
+	@Test
+	public void testOneGroupOneRecordLink() throws Exception {
+		DataRecordLinkSpy recordLink = new DataRecordLinkSpy();
+		recordLink.MRV.setDefaultReturnValuesSupplier("getNameInData", () -> "someRecordLink");
+		dataGroup.MRV.setDefaultReturnValuesSupplier("hasChildren", () -> true);
+		dataGroup.MRV.setDefaultReturnValuesSupplier("getChildren", () -> List.of(recordLink));
+
+		String definition = writer.writeDefinitionFromUsingDataChild(dataGroup);
+		String expectedDefinition = """
+				someGroup(group)
+					someRecordLink(recordLink, 1-1, noConstraint)""";
+
+		assertEquals(definition, expectedDefinition);
+	}
+
+	@Test
+	public void testOneGroupOneResourceLink() throws Exception {
+		DataResourceLinkSpy resourceLink = new DataResourceLinkSpy();
+		resourceLink.MRV.setDefaultReturnValuesSupplier("getNameInData", () -> "someResourceLink");
+		dataGroup.MRV.setDefaultReturnValuesSupplier("hasChildren", () -> true);
+		dataGroup.MRV.setDefaultReturnValuesSupplier("getChildren", () -> List.of(resourceLink));
+
+		String definition = writer.writeDefinitionFromUsingDataChild(dataGroup);
+		String expectedDefinition = """
+				someGroup(group)
+					someResourceLink(resourceLink, 1-1, noConstraint)""";
+
+		assertEquals(definition, expectedDefinition);
+	}
+
+	@Test
+	public void testNestedGroupsWithOneChildEach() throws Exception {
+		DataResourceLinkSpy resourceLink = new DataResourceLinkSpy();
+		resourceLink.MRV.setDefaultReturnValuesSupplier("getNameInData", () -> "someResourceLink");
+		dataGroup.MRV.setDefaultReturnValuesSupplier("hasChildren", () -> true);
+
+		DataGroupSpy childGroup = createDataGroupSpy("childGroup");
+		DataAtomicSpy textVariable = new DataAtomicSpy();
+		textVariable.MRV.setDefaultReturnValuesSupplier("getNameInData", () -> "someTextVariable");
+		childGroup.MRV.setDefaultReturnValuesSupplier("hasChildren", () -> true);
+
+		DataGroupSpy childGroup2 = createDataGroupSpy("childGroup2");
+		DataAtomicSpy textVariable2 = new DataAtomicSpy();
+		textVariable2.MRV.setDefaultReturnValuesSupplier("getNameInData",
+				() -> "someTextVariable2");
+		childGroup2.MRV.setDefaultReturnValuesSupplier("hasChildren", () -> true);
+		childGroup2.MRV.setDefaultReturnValuesSupplier("getChildren", () -> List.of(textVariable2));
+
+		childGroup.MRV.setDefaultReturnValuesSupplier("getChildren",
+				() -> List.of(textVariable, childGroup2));
+		dataGroup.MRV.setDefaultReturnValuesSupplier("getChildren",
+				() -> List.of(resourceLink, childGroup));
+
+		String definition = writer.writeDefinitionFromUsingDataChild(dataGroup);
+		String expectedDefinition = """
+				someGroup(group)
+					someResourceLink(resourceLink, 1-1, noConstraint)
+					childGroup(group)
+						someTextVariable(textVariable, 1-1, noConstraint)
+						childGroup2(group)
+							someTextVariable2(textVariable, 1-1, noConstraint)""";
+
 		assertEquals(definition, expectedDefinition);
 	}
 
