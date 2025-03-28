9cdc3ec012f8399074030c415b434f75f3f66745
olovm <olov.mckie@ub.uu.se>
2025-03-28 17:50:48 +0000

    trying to find out why the test does not work on build

--- src/main/java/se/uu/ub/cora/fitnesseintegration/script/FileReader.java
+++ src/main/java/se/uu/ub/cora/fitnesseintegration/script/FileReader.java
@@ -18,11 +18,8 @@
  */
 package se.uu.ub.cora.fitnesseintegration.script;
 
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Optional;
 
 import se.uu.ub.cora.fitnesseintegration.file.ArchiveFileReaderImp;
@@ -37,46 +34,12 @@ public class FileReader {
 	}
 
 	public boolean fileWithNameRemovedFromPath(String fileName, String path) {
-		System.err.println("what1");
-
-		// Specify the directory path
-		Path dirPath = Paths.get(path);
-		System.err.println("what2");
-
-		// Check if the directory exists
-		if (Files.exists(dirPath) && Files.isDirectory(dirPath)) {
-			System.err.println("what3");
-			try (DirectoryStream<Path> stream = Files.newDirectoryStream(dirPath)) {
-				System.err.println("what4");
-
-				for (Path entry : stream) {
-					System.err.println("what5");
-					System.out.println(entry.getFileName());
-					System.err.println("what6");
-				}
-			} catch (IOException e) {
-				System.err.println("what7");
-				System.err.println("Error reading directory: " + e.getMessage());
-			}
-		} else {
-			System.err.println("what8");
-			System.out.println("The specified path is not a directory or does not exist.");
-		}
-
 		fileExists = true;
-		System.err.println("what9");
 		Path filePath = Path.of(path, fileName);
-		System.err.println("what10");
 		Waiter waiter = DependencyProvider.factorWaiter();
-		System.err.println(waiter.getClass());
 
-		System.err.println("what11");
 		return waiter.waitUntilConditionFullfilled(() -> {
-			System.err.println("what12");
 			fileExists = Files.exists(filePath);
-			System.err.println("what13");
-			System.out.println(fileExists);
-			System.err.println("what14");
 		}, () -> !fileExists, 500, 10);
 	}
 
